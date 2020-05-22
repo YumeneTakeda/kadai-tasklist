63a286f1cb79aa49bd0ac95a26e2812911f0b887
yumene.takeda <yuyuyukoppepan@gmail.com>
2020-05-22 14:58:15 +0000

    refactor:delete extra_routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,6 @@ Rails.application.routes.draw do
   get 'sessions/destroy'
   root to: "tasks#index"
   
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
